1c744622607cdfd3c3fc063ece8a4b781e1876db
koshkayaa <koshkayaa@proton.me>
2025-10-28 03:27:41 +0800

    Add page.tsx

---
+++ packages/site/app/page.tsx
@@ -0,0 +1,11 @@
+import { DiceNarration } from "@/components/DiceNarration";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
+        <DiceNarration />
+      </div>
+    </main>
+  );
+}
