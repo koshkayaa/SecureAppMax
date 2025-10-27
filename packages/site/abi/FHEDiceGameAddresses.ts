5d39b0c10c53676970464c32077f30263c1d4a13
koshkayaa <koshkayaa@proton.me>
2025-10-28 03:27:36 +0800

    Add FHEDiceGameAddresses.ts

---
+++ packages/site/abi/FHEDiceGameAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  By commands: 'npx hardhat deploy' or 'npx hardhat node'
+*/
+export const FHEDiceGameAddresses = { 
+  "11155111": { address: "0x31630746e870D3D57e8B708ac479f269e1Da4c10", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
+};
